d91defef3c842811b27649ec680b24f309a2c3b2
Madha Saikumar <105370858+Madhasai21@users.noreply.github.com>
2022-09-25 19:20:54 +0530

    Update GroceryRepository.kt

--- GroceryApp-main (1)/GroceryApp-main/app/src/main/java/com/madhasaikumar/groceryapp/GroceryRepository.kt
+++ GroceryApp-main (1)/GroceryApp-main/app/src/main/java/com/madhasaikumar/groceryapp/GroceryRepository.kt
@@ -1,4 +1,4 @@
-package com.divyanshu.groceryapp
+package com.madhasaikumar.groceryapp
 
 class GroceryRepository(private val db:GroceryDatabase) {
     suspend fun insert(items: GroceryItems) = db.getGroceryDao().insert(items)
